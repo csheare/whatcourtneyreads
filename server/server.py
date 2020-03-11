9b0b19197f8ceaf56b37047558819723c3148909
Courtney Shearer <courtney.a.shearer@gmail.com>
2020-03-10 20:39:39 -0700

    fetch and axios compare really

--- server/server.py
+++ server/server.py
@@ -1,11 +1,14 @@
 from flask import Flask, request
 from flask_cors import CORS
+import json
+from flask import jsonify
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def test():
-	player, move = request.args['player'], request.args['move']
+	body = json.loads(request.data)
+	player, move = body['player'], body['move']
 	print(f'Player {player} played an {move}')
-	return ''
+	return jsonify(body)
