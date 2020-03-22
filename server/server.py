5ae9231bc7b381fb268a7fe152bb2c4671f9f3b0
Courtney Shearer <courtney.a.shearer@gmail.com>
2020-03-21 22:00:12 -0700

    updated the config for sqlalchemy and postgres goodies

--- server/server.py
+++ server/server.py
@@ -1,10 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+
 import json
-from flask import jsonify
+import os
 
 app = Flask(__name__)
 CORS(app)
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['POST'])
 def test():
