d1ada23e20c442271fe6322832760bfbf2363d3b
Courtney Shearer <courtney.a.shearer@gmail.com>
2020-03-21 22:16:03 -0700

    this data model is brought to you by Donna Summer

---
+++ book.py
@@ -0,0 +1,21 @@
+from app import db
+from sqlalchemy.dialects.postgresql import JSON
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String())
+    author = db.Column(db.String())
+    start = db.Column(db.Date())
+    end = db.Column(db.Date())
+
+    def __init__(self, title, author, start, end):
+        self.title = title
+        self.author = author
+        self.start = start
+        self.end = end
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
