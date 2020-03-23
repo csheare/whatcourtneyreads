f1cd6fb9d01da89ca3586b062ebb5a0f838d3db4
Courtney Shearer <courtney.a.shearer@gmail.com>
2020-03-22 22:40:44 -0700

    bare bones

---
+++ client/src/book.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from "axios";
+import './index.css';
+
+class Timeline extends React.Component {
+
+}
+
+// ========================================
+
+ReactDOM.render(
+	<Timeline />,
+	document.getElementById('root')
+);
